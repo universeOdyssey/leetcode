f1e09152396bbc34a0b7b25154046685ff113155
wangyu < alan.wang@azt-china.com>
2018-01-02 15:25:35 +0800

    二分查找

---
+++ src/com/ironwyu/leetcode/SearchInsertPosition35/SearchInsertPosition.java
@@ -0,0 +1,54 @@
+package com.ironwyu.leetcode.SearchInsertPosition35;
+
+/**
+ * Given a sorted array and a target value, return the index if the target is found. If not,
+ * return the index where it would be if it were inserted in order.
+ * You may assume no duplicates in the array.
+ */
+
+public class SearchInsertPosition {
+
+    public int searchInsert2(int[] nums, int target) {
+        int i = 0;
+        int j = 0;
+        for(i = 0; i< nums.length; i++){
+            if(target == nums[i]) {
+                return i;
+            }
+        }
+
+        for(j = 0; j< nums.length; j++){
+            if(nums[j]> target){
+                return j;
+            }
+        }
+        return j;
+
+    }
+
+    public int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length-1;
+
+        while(start<=end) {
+            int mid = (start + end) / 2;
+            if(nums[mid]<target) {
+                start = mid + 1;
+            } else if(nums[mid]>target) {
+                end = mid - 1;
+            } else
+                return mid;
+        }
+
+        return start;
+    }
+
+    public static void main(String[] args) {
+        // [1,3,5,6]-7  [1]-1   [1,3]-2 [1,3,5]-4 [3,5,7,9,10]-8
+        int [] nums = {1,3,5,6};
+        int target = 7;
+        int result = new SearchInsertPosition().searchInsert(nums, target);
+        System.out.println(result);
+    }
+
+}
---
+++ src/com/ironwyu/leetcode/SearchinRotatedSortedArray33/SearchinRotatedSortedArray.java
@@ -0,0 +1,58 @@
+package com.ironwyu.leetcode.SearchinRotatedSortedArray33;
+
+/**
+ *
+ Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+ (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+ You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+ You may assume no duplicate exists in the array.
+
+ */
+
+public class SearchinRotatedSortedArray {
+
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end)/2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] < nums[end]){
+                if (target > nums[mid] && target <= nums[end]){
+                    start = mid + 1;
+                }else{
+                    end = mid - 1;
+                }
+            }else{
+                if (target >= nums[start] && target < nums[mid]){
+                    end = mid - 1;
+                }else{
+                    start = mid + 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     *
+     （1）如果target==A[m]，那么m就是我们要的结果，直接返回；
+     （2）如果A[m]<A[r]，那么说明从m到r一定是有序的（没有受到rotate的影响），那么我们只需要判断target是不是在m到r之间，如果是则把左边缘移到m+1，否则就target在另一半，即把右边缘移到m-1。
+     （3）如果A[m]>=A[r]，那么说明从l到m一定是有序的，同样只需要判断target是否在这个范围内，相应的移动边缘即可。
+     */
+}
