738d4ab11ddda488bbf7edf6fea3960fab16e65a
wangyu < alan.wang@azt-china.com>
2018-01-03 17:30:28 +0800

    二分查找

---
+++ src/com/ironwyu/leetcode/SearchforaRange34/SearchforaRange34.java
@@ -0,0 +1,60 @@
+package com.ironwyu.leetcode.SearchforaRange34;
+
+/**
+ Given an array of integers sorted in ascending order, find the starting and ending position of a given target value.
+
+ Your algorithm's runtime complexity must be in the order of O(log n).
+
+ If the target is not found in the array, return [-1, -1].
+
+ For example,
+ Given [5, 7, 7, 8, 8, 10] and target value 8,
+ return [3, 4].
+ */
+
+public class SearchforaRange34 {
+
+    // returns leftmost (or rightmost) index at which `target` should be
+    // inserted in sorted array `nums` via binary search.
+    private int extremeInsertionIndex(int[] nums, int target, boolean left) {
+        int lo = 0;
+        int hi = nums.length;
+
+        while (lo < hi) {
+            int mid = (lo+hi)/2;
+            if (nums[mid] > target || (left && target == nums[mid])) {
+                hi = mid;
+            }
+            else {
+                lo = mid+1;
+            }
+        }
+
+        return lo;
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        int[] targetRange = {-1, -1};
+
+        int leftIdx = extremeInsertionIndex(nums, target, true);
+
+        // assert that `leftIdx` is within the array bounds and that `target`
+        // is actually in `nums`.
+        if (leftIdx == nums.length || nums[leftIdx] != target) {
+            return targetRange;
+        }
+
+        targetRange[0] = leftIdx;
+        targetRange[1] = extremeInsertionIndex(nums, target, false)-1;
+
+        return targetRange;
+    }
+
+    public static void main(String[] args) {
+        // [1,3,5,6]-7  [1]-1   [1,3]-2 [1,3,5]-4 [3,5,7,9,10]-8
+        int [] nums = {1,3,3,5,5,7};
+        int target = 5;
+        int [] result = new SearchforaRange34().searchRange(nums, target);
+        System.out.println(result);
+    }
+}
---
+++ src/com/ironwyu/leetcode/SearchinRotatedSortedArrayII81/SearchinRotatedSortedArrayII.java
@@ -0,0 +1,54 @@
+package com.ironwyu.leetcode.SearchinRotatedSortedArrayII81;
+
+
+/**
+ Follow up for "Search in Rotated Sorted Array":
+ What if duplicates are allowed?
+
+ Would this affect the run-time complexity? How and why?
+ */
+
+public class SearchinRotatedSortedArrayII {
+
+    public boolean search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end){
+            int mid = (start + end)/2;
+            if (nums[mid] == target){
+                return true;
+            }
+            if(nums[mid] > nums[start]){
+                if(target >= nums[start] && target <= nums[mid]){
+                    end = mid - 1;
+                }else{
+                    start = mid + 1;
+                }
+            } else if (nums[mid] < nums[start]){
+                if (target <= nums[end] && target >= nums[mid]) {
+                    start = mid + 1;
+                }else{
+                    end = mid - 1;
+                }
+            } else{
+                start++;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        // [1,3,5,6]-7  [1]-1   [1,3]-2 [1,3,5]-4 [3,5,7,9,10]-8
+        int [] nums = {1,3,5};
+        int target = 1;
+        boolean result = new SearchinRotatedSortedArrayII().search(nums, target);
+        System.out.println(result);
+    }
+
+    /**
+     在SearchinRotatedSortedArray基础上移位过滤掉重复的边界
+     */
+}
