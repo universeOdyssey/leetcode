9b65763e45f0f9a3541a2ba55d7f083c348b3947
wangyu < alan.wang@azt-china.com>
2018-01-11 17:25:14 +0800

    Array

---
+++ src/com/ironwyu/leetcode/ContainerWithMostWater11/ContainerWithMostWater11.java
@@ -0,0 +1,23 @@
+package com.ironwyu.leetcode.ContainerWithMostWater11;
+
+/**
+ Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai).
+ n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines,
+ which together with x-axis forms a container, such that the container contains the most water.
+
+ Note: You may not slant the container and n is at least 2.
+
+ !tag : Array
+ */
+
+public class ContainerWithMostWater11 {
+
+    public int maxArea(int[] height) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
